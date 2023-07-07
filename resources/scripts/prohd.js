d7e4d3c50672321f7e71207884e5217dc988ba07
Magdalena Turska <tuurma@gmail.com>
2023-07-07 16:13:18 +0200

    Copy citation to clipboard

--- resources/scripts/prohd.js
+++ resources/scripts/prohd.js
@@ -1,8 +1,20 @@
 window.addEventListener('load', function() {
+
+    // handle jumping to the selected page from the toolbar
     document.querySelector('#submit').addEventListener('click', function() {
         const input = document.getElementById('gotoPage');
-        // console.log('gotopage %o', input.value);
-
         pbEvents.emit('pb-refresh', 'transcription', {"root": input.value});
     });
+
+    // add copy-to-clipboard functionality for the citation button
+    const view = document.getElementById('commentary');
+    if (view) {
+        view.addEventListener('pb-update', function(ev) {
+            const clipTrigger = ev.detail.root.querySelector('#clipboard-trigger');
+            const clipContent = ev.detail.root.querySelector('#clipboard-citation');
+            clipTrigger.addEventListener('click', function() {
+                navigator.clipboard.writeText(clipContent.innerText);
+            });
+        });
+    }
 });
